1bf9dcf44fa03c952af24782187e088dce0f256f
John Wells <molekewl2017@gmail.com>
2021-03-03 13:32:16 -0800

    logic manipulating dom elements

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+document.querySelector('.check').addEventListener('click', function() {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    // if no input & user hits enter
+    document.querySelector('.message').textContent = 'No number!';
+  }
+});

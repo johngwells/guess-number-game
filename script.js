f0d58b95b688d2c9f7ca6d08891a9ed1b56016fa
John Wells <molekewl2017@gmail.com>
2021-03-04 14:00:02 -0800

    game logic & reset

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
 'use strict';
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = secretNumber;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
 
 
 document.querySelector('.check').addEventListener('click', function() {
-  const guess = Number(document.querySelector('.guess').value);
+  let guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
   if (!guess) {
     // if no input & user hits enter
     document.querySelector('.message').textContent = 'No number!';
   } else if (guess === secretNumber) {
+    // Player wins
+    document.querySelector('.number').textContent = secretNumber;
     document.querySelector('.message').textContent = 'Correct Number!'
+    document.querySelector('body').style.backgroundColor = '#60b347'
+    document.querySelector('.number').style.width = '30rem'
+
   } else if (guess > secretNumber) {
+    // user guess is too high
     if (score > 1) {
       document.querySelector('.message').textContent = 'Guess to high';
       score--;
@@ -25,6 +30,7 @@ document.querySelector('.check').addEventListener('click', function() {
       document.querySelector('.score').textContent = 0;
     }
   } else if (guess < secretNumber) {
+    // user guess is too low
     if (score > 1) {
       document.querySelector('.message').textContent = 'Guess to low';
       score--;
@@ -34,4 +40,14 @@ document.querySelector('.check').addEventListener('click', function() {
       document.querySelector('.score').textContent = 0;
     }
   }
+});
+
+document.querySelector('.again').addEventListener('click', function() {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.score').textContent = 20;
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').textContent = '';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.message').textContent = 'start guessing...';
 });

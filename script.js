019bf65dec8810581ba15e54e9e1979ea238097f
John Wells <molekewl2017@gmail.com>
2021-03-04 22:03:01 -0800

    save highscore & refactor | finished

--- script.js
+++ script.js
@@ -3,7 +3,11 @@
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
+let highscore = 0;
 
+const displayMessage = function(message) {
+  document.querySelector('.message').textContent = message;
+}
 
 document.querySelector('.check').addEventListener('click', function() {
   let guess = Number(document.querySelector('.guess').value);
@@ -14,29 +18,22 @@ document.querySelector('.check').addEventListener('click', function() {
     document.querySelector('.message').textContent = 'No number!';
   } else if (guess === secretNumber) {
     // Player wins
-    document.querySelector('.number').textContent = secretNumber;
-    document.querySelector('.message').textContent = 'Correct Number!'
+    displayMessage('Correct Number!')
     document.querySelector('body').style.backgroundColor = '#60b347'
     document.querySelector('.number').style.width = '30rem'
 
-  } else if (guess > secretNumber) {
-    // user guess is too high
-    if (score > 1) {
-      document.querySelector('.message').textContent = 'Guess to high';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = 'Game Over';
-      document.querySelector('.score').textContent = 0;
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
     }
-  } else if (guess < secretNumber) {
-    // user guess is too low
+
+  } else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = 'Guess to low';
+      document.querySelector('.message').textContent = guess > secretNumber ? 'Guess too high' : 'Guess too low';
       score--;
       document.querySelector('.score').textContent = score;
     } else {
-      document.querySelector('.message').textContent = 'Game Over';
+      displayMessage('Game Over');
       document.querySelector('.score').textContent = 0;
     }
   }
@@ -51,5 +48,5 @@ document.querySelector('.again').addEventListener('click', function() {
   document.querySelector('.number').textContent = '?';
   document.querySelector('.guess').value = '';
   document.querySelector('.number').style.width = '15rem';
-  document.querySelector('.message').textContent = 'start guessing...';
+  displayMessage('start guessing...');
 });
